c04e09fbe7375faa2ee73923495a721716e3fa7d
epittab <ep7m@me.com>
2020-09-17 15:41:42 -0400

    more on methods

--- Methods/Methods.java
+++ Methods/Methods.java
@@ -3,15 +3,44 @@ package Methods;
 public class Methods {
     public static void main(String[] args) {
 
-        calculateScore(true, 800, 5, 100);
-        calculateScore(true, 10000, 8, 200);
+        int gameScore = calculateScore(true, 800, 5, 100);
+        int secondGameScore = calculateScore(true, 10000, 8, 200);
+
+        displayHighScorePosition("Enrique", calculateHighScorePosition(1500));
+        displayHighScorePosition("John", calculateHighScorePosition(900));
+        displayHighScorePosition("Michael", calculateHighScorePosition(400));
+        displayHighScorePosition("Anne", calculateHighScorePosition(50));
+
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
+            return finalScore;
+        }
+        return -1;
+
+    }
+
+    public static void displayHighScorePosition(String name, int position){
+        System.out.println(name + " managed to get into position " + position + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int score){
+        if ( score > 1000) {
+            return 1;
+        } else if ( score > 500 ) {
+            return 2;
+        } else if ( score > 100 ) {
+            return 3;
+        } else {
+            return 4;
         }
     }
+
+
+
 }
