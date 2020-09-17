ade3a2a185d9e6829da98204309affd4d0dda667
epittab <ep7m@me.com>
2020-09-17 13:44:25 -0400

    added if...then

---
+++ IfThen/IfThen.java
@@ -0,0 +1,14 @@
+package IfThen;
+
+public class IfThen {
+
+    public static void main(String[] args) {
+        boolean isAlien = false;
+
+        if (isAlien) {
+            System.out.println("THis is an alien.");
+        } else {
+            System.out.println("THis is NOT an alien.");
+        }
+    }
+}
